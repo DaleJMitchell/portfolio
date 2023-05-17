cc904b0ec5e824f865d5ebdaca31356ee29757e4
DaleJMitchell <dalejmitchell@icloud.com>
2023-05-17 16:35:54 -0400

    Added button placment and hover effects.

--- src/components/PageNavigation.vue
+++ src/components/PageNavigation.vue
@@ -1,25 +1,34 @@
 <template>
-   
-  
-  <div id="myTopnav" class="topnav h-screen text-white flex flex-col justify-center items-end">
-    <div class="logo-container">
-      <img src="../assets/2023.03.28 Dale Mitchel Headshot-square-centered.jpg" alt="Dale Mitchell Headshot" class="rounded-full w-full">
-      
+  <div class="page-container">
+    <div class="left-section">
+      <div class="navigation-buttons">
+        <a href="#home" class="home">Home</a>
+        <a href="#news" class="news">News</a>
+        <a href="#contact" class="contact">Contact</a>
+        <a href="#about" class="about">About</a>
     </div>
-
-    <a class="active px-2 rounded-l-xl my-1 hover:text-red-500" href="#home">Home</a>
-    <a class="px-2 rounded-l-xl my-1 hover:text-yellow-500" href="#news">News</a>
-    <a class="px-2 rounded-l-xl my-1 hover:text-green-500" href="#contact">Contact</a>
-    <a class="px-2 rounded-l-xl my-1 hover:text-blue-500" @click="navigateToAboutPage">About</a>
-    
+      <div class="logo-container">
+        <img src="../assets/2023.03.28 Dale Mitchel Headshot-square-centered.jpg" alt="Dale Mitchell Headshot" class="rounded-full w-full">
+      </div>
+    </div>
+    <div class="right-section">
+    </div>
   </div>
-
 </template>
 
 <script>
 export default {
   name: "PageNavigation",
   methods: {
+    navigateToHomePage() {
+      this.$router.push('/home');
+    },
+    navigateToNewsPage() {
+      this.$router.push('/news');
+    },
+    navigateToContactPage() {
+      this.$router.push('/contact');
+    },
     navigateToAboutPage() {
       this.$router.push('/about');
     }
@@ -28,17 +37,66 @@ export default {
 </script>
 
 <style>
-  .topnav a {
-    padding: 10px;
-    transition: background-color 0.3s ease;
-  }
-  
-  .topnav a:hover {
-    background-color: rgb(255, 255, 255);
-    border-radius: 0 -10px -10px 0;
-  }
+.page-container{
+  display: flex;
+  height: 100vh;
+}
 
-  .logo-container {
+.left-section {
+  flex: 1;
+  background-color: gray;
+  /* padding: 20px; */
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end
+}
+.navigation-buttons{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.navigation-buttons a {
+  margin-bottom: 5px;
+  color: black;
+  text-decoration: none;
+  padding: 20px; 
+  border: 1px solid transparent;
+  padding-right: 20px;
+
+}
+.navigation-buttons a:hover {
+  transition: border-color 0.3s; 
+  transition: background-color 0.3s; 
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  background-color: white;
+  border-color: white;
+}
+.navigation-buttons a.home:hover {
+  color: red; /* Color when hovering home */
+}
+
+.navigation-buttons a.news:hover {
+  color: green; /* Color when hovering news */
+}
+
+.navigation-buttons a.contact:hover {
+  color: blue; /* Color when hovering contact */
+}
+
+.navigation-buttons a.about:hover {
+  color: purple; /* Color when hovering about */
+}
+
+.right-section{
+  flex: 1;
+  background-color: white;
+  padding: 20px;
+}
+
+
+.logo-container {
     position: absolute;
     top: 2in;
     left: 0.5in;
